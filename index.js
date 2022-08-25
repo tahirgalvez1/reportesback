ea705b80cc6fc0cdfcdf7e3ba6b84a0455e99ad9
tahirgalvez1 <tahir.galvez@hotmail.com>
2022-08-25 10:23:32 -0700

    req

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-const cool = require('cool-ascii-faces');
 const express = require('express');
 const path = require('path');
 const PORT = process.env.PORT || 5000;
 
 express()
   .get('/', (req, res) => res.send('running'))
-  .get('/cool', (req, res) => res.send(cool()))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
